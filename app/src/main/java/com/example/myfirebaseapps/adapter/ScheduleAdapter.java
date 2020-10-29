a8112757534f2e693d07d2514903ea024beef71a
ReinerJasin <reinerjasin@gmail.com>
2020-10-29 21:26:14 +0800

    Schedule Adapter HARUSNYA sudah

--- app/src/main/java/com/example/myfirebaseapps/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/example/myfirebaseapps/adapter/ScheduleAdapter.java
@@ -1,31 +1,97 @@
 package com.example.myfirebaseapps.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myfirebaseapps.Model.Course;
+import com.example.myfirebaseapps.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.CardViewViewHolder> {
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+    private Context context;
+
+    private ArrayList<Course> listCourse;
+
+    public ArrayList<Course> getListCourse() {
+        return listCourse;
+    }
+
+    public void setListCourse(ArrayList<Course> listCourse) {
+        this.listCourse = listCourse;
+    }
+
+    public ScheduleAdapter(Context context) {
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public CardViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.enroll_adapter, parent, false);
+        return new ScheduleAdapter.CardViewViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CardViewViewHolder holder, int position) {
+        final Course course = getListCourse().get(position);
 
+        holder.enroll_course.setText(course.getSubject());
+        holder.enroll_lecturer.setText(course.getLecturer());
+        holder.enroll_day.setText(course.getDay());
+        holder.enroll_time.setText(course.getStart());
+        holder.enroll_time_end.setText(course.getEnd());
+
+        holder.enroll_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("student").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("course").child(course.getId()).removeValue(new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return getListCourse().size();
     }
 
     public class CardViewViewHolder extends RecyclerView.ViewHolder {
+
+        TextView enroll_course, enroll_lecturer, enroll_day, enroll_time, enroll_time_end;
+        ImageButton enroll_button;
+
         public CardViewViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            enroll_course = itemView.findViewById(R.id.enroll_course);
+            enroll_lecturer = itemView.findViewById(R.id.enroll_lecturer);
+            enroll_day = itemView.findViewById(R.id.enroll_day);
+            enroll_time = itemView.findViewById(R.id.enroll_time);
+            enroll_time_end = itemView.findViewById(R.id.enroll_time_end);
+            enroll_button = itemView.findViewById(R.id.enroll_imageButton_add);
+
+            enroll_button.setBackgroundResource(R.drawable.ic_baseline_delete_24);
+
         }
     }
 }
